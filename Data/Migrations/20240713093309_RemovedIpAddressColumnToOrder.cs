25cc71fd52065824d90b9d31de92631f92fc3d5f
georgi2005atanasov <gogida9876@abv.bg>
2024-07-13 12:34:54 +0300

    Tried to add ip address but i think localhost does not get what ip address i want to get.

---
+++ Data/Migrations/20240713093309_RemovedIpAddressColumnToOrder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NutriBest.Server.Data.Migrations
+{
+    public partial class RemovedIpAddressColumnToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IpAddress",
+                table: "Orders");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IpAddress",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
